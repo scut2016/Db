297dcf154e8bf4fbde06104dd832e6642110924c
tan <1002335644@qq.com>
2016-10-08 13:52:51 +0800

    谭庭超

--- Db/MySQL.php
+++ Db/MySQL.php
@@ -7,16 +7,19 @@ class MySQL implements IDatabase
     function connect()
     {
         // TODO: Implement connect() method.
+        echo "hello world";
     }
 
     function query()
     {
         // TODO: Implement query() method.
+        echo "hi world";
     }
 
     function close()
     {
         // TODO: Implement close() method.
+        echo "how are you";
     }
 
 }
